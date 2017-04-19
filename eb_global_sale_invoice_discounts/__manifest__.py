214872496c4de70d552326b510cd721487fd92b8
Simplify-ERP® <github@euroblaze.de>
2017-04-19 10:36:43 +0200

    Update __manifest__.py

--- eb_global_sale_invoice_discounts/__manifest__.py
+++ eb_global_sale_invoice_discounts/__manifest__.py
@@ -23,6 +23,7 @@
     'name': 'Global Sale Order Invoice Discount',
     'description': 'Manage Discount on Sale Order  and Invoice ',
     'summary': '',
+    'images': ['static/description/banner.jpeg'],
     'category': 'Accounting & Sales ',
     'version': '10.0.1.0',
     'website': 'www.euroblaze.de',
