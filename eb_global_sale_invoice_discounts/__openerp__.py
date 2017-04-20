b9512bfbf66c0fd04ab52b295506f7c0cfd6d6c8
Simplify-ERP® <github@euroblaze.de>
2017-04-20 11:52:08 +0200

    Update __openerp__.py

--- eb_global_sale_invoice_discounts/__openerp__.py
+++ eb_global_sale_invoice_discounts/__openerp__.py
@@ -20,13 +20,14 @@
 #
 ##############################################################################
 {
-    'name': 'eb_sales_invoices_discounts',
+    'name': 'Global Sale Order Invoice Discount',
     'description': 'Manage Discount on Sale Order  and Invoice ',
     'summary': '',
     'category': 'Accounting & Sales ',
     'version': '10.0.1.0',
+    'images' : ['static/description/banner.jpg'].
     'website': 'www.euroblaze.de',
-    'author': 'Geetha Reddy',
+    'author': 'odoo@euroblaze.de',
     'depends': ['base', 'account', 'account_accountant', 'sale'],
     'data': [
         'views/views.xml',
